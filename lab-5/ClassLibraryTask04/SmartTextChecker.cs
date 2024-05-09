cf57cfa6ba0674db99e9d98d303a31f5caa706ee
Денис Козачок <ipz224_kdp@student.ztu.edu.ua>
2024-05-09 19:33:51 +0300

    Клонування завдань для 5 лаб

---
+++ lab-5/ClassLibraryTask04/SmartTextChecker.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibraryTask04
+{
+    public class SmartTextChecker : ITextReader
+    {
+        private readonly SmartTextReader _textReader;
+
+        public SmartTextChecker()
+        {
+            _textReader = new SmartTextReader();
+        }
+
+        public string[][] ReadText(string filePath)
+        {
+            Console.WriteLine("Open file");
+
+            var result = _textReader.ReadText(filePath);
+
+            Console.WriteLine("The file was read successfully!");
+            Console.WriteLine($"Number of rows: {result.Length}");
+
+            int totalChars = 0;
+            foreach (var line in result)
+            {
+                totalChars += line.Length;
+            }
+
+            Console.WriteLine($"The number of letters: {totalChars}");
+            Console.WriteLine("Close file");
+
+            return result;
+        }
+    }
+}
