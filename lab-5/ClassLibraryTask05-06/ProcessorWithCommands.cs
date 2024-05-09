2e8061bef49219e55dbec031feda68435cb301a0
Денис Козачок <ipz224_kdp@student.ztu.edu.ua>
2024-05-09 20:39:18 +0300

    Створення класів для взаємодії з елементами сторінки використовуючи Шаблон Команди

---
+++ lab-5/ClassLibraryTask05-06/ProcessorWithCommands.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibraryTask05_06
+{
+    class HtmlProcessorWithCommands
+    {
+        private readonly List<AddClassCommand> _commands = new List<AddClassCommand>();
+
+        public void AddClass(string element, string classToAdd)
+        {
+            AddClassCommand command = new AddClassCommand(element, classToAdd);
+            _commands.Add(command);
+        }
+
+        public void ExecuteCommands()
+        {
+            foreach (var command in _commands)
+            {
+                command.Execute();
+            }
+        }
+
+        public void UndoCommands()
+        {
+            for (int i = _commands.Count - 1; i >= 0; i--)
+            {
+                _commands[i].Undo();
+            }
+        }
+    }
+}
