06380577364c32b9c46bf1a76770bfea533d73ae
Денис Козачок <ipz224_kdp@student.ztu.edu.ua>
2024-05-09 21:08:44 +0300

    Створення класів для перегляду стану елементів HTML використовуючи Шаблон (State)

--- lab-5/ClassLibraryTask05-06/HTMLElement.cs
+++ lab-5/ClassLibraryTask05-06/HTMLElement.cs
@@ -1,8 +1,42 @@
 ﻿using System;
-
-public class Class1
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace ClassLibraryTask05_06
 {
-	public Class1()
-	{
-	}
+    class HtmlElement
+    {
+        private ElementState _state;
+        private readonly string _element;
+
+        public HtmlElement(string element)
+        {
+            _element = element;
+            SetInactiveState();
+        }
+
+        private void SetInactiveState()
+        {
+            _state = new InactiveState(_element);
+            _state.Handle();
+        }
+
+        private void SetActiveState()
+        {
+            _state = new ActiveState(_element);
+            _state.Handle();
+        }
+
+        public void Activate()
+        {
+            SetActiveState();
+        }
+
+        public void Deactivate()
+        {
+            SetInactiveState();
+        }
+    }
+
 }
