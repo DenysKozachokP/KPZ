7471d8ecf46a66c1b3a540c06884b1b0e2da0a04
Денис Козачок <ipz224_kdp@student.ztu.edu.ua>
2024-05-09 20:23:27 +0300

    Створення ітератору для елементыв ХТМЛ використовуючи шаблон ітератора

---
+++ lab-5/ClassLibraryTask05-06/AHtmlProcessor.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+namespace ClassLibraryTask05_06
+{
+    abstract class HtmlProcessor
+    {
+        public void ProcessHtml(string html)
+        {
+            List<string> elements = ExtractElements(html);
+            foreach (var element in elements)
+            {
+                ProcessElement(element);
+            }
+        }
+        protected virtual List<string> ExtractElements(string html)
+        {
+            return new List<string>();
+        }
+        protected abstract void ProcessElement(string element);
+    }
+}
---
+++ lab-5/ClassLibraryTask05-06/HtmlProcessor.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+namespace ClassLibraryTask05_06
+{
+    class CustomHtmlProcessor : HtmlProcessor
+    {
+        protected override List<string> ExtractElements(string html)
+        {
+            List<string> elements = new List<string>();
+
+            int startIndex = 0;
+            while (true)
+            {
+
+                int openIndex = html.IndexOf('<', startIndex);
+                int closeIndex = html.IndexOf('>', startIndex);
+
+                if (openIndex == -1 || closeIndex == -1)
+                    break;
+
+                elements.Add(html.Substring(openIndex, closeIndex - openIndex + 1));
+
+
+                startIndex = closeIndex + 1;
+            }
+
+            return elements;
+        }
+        protected override void ProcessElement(string element)
+        {
+            Console.WriteLine($"Processing element: {element}");
+        }
+    }
+}
